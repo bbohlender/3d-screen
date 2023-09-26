f05273c0de37741c608377ef540bdf04f646d817
Bela Bohlender <bbohlender@coconut-capital.com>
2023-09-27 00:45:18 +0200

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/3d-screen"
+    base: "/3d-screen",
+    build: {
+        target: "esnext"
+    }
 });
